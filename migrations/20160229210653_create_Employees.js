2c29b73f7acc3c3aee04e9837417566e48025bc0
Michael Leffert <michael.leffert@seisan.com>
2016-03-16 20:08:56 -0400

    tweak to migration

--- migrations/20160229210653_create_Employees.js
+++ migrations/20160229210653_create_Employees.js
@@ -1,28 +1,28 @@
 
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('departments', function(table){
-      table.increments('depId').primary();
-      table.string('name');
-  }).createTable('jobs', function(table){
-      table.increments('jobId').primary();
-      table.string('title');
-      table.string('description');
-      table.integer('function');
-      table.integer('departmentId').references(departments.id)
-  }).createTable('employees', function(table){
-      table.increments('empId').primary();
-      table.string('fName');
-      table.string('lName');
-      table.string('street');
-      table.string('city');
-      table.string('state');
-      table.integer('zip');
-      table.string('phone');
-      table.string('email');
-      table.integer('jobId').references('jobs.id');
-      table.double('hourlyWage');
-      table.double('salary');
-  });
+    return knex.schema.createTable('departments', function(table){
+        table.increments('depId').primary();
+        table.string('name');
+    }).createTable('jobs', function(table){
+        table.increments('jobId').primary();
+        table.string('title');
+        table.string('description');
+        table.integer('function');
+        table.integer('departmentId').unsigned().references('departments.depId');
+    }).createTable('employees', function(table){
+        table.increments('empId').primary();
+        table.string('fName');
+        table.string('lName');
+        table.string('street');
+        table.string('city');
+        table.string('state');
+        table.integer('zip');
+        table.string('phone');
+        table.string('email');
+        table.integer('jobId').unsigned().references('jobs.jobId');
+        table.double('hourlyWage');
+        table.double('salary');
+    });
 };
 
 exports.down = function(knex, Promise) {
